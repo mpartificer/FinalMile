cdd832a5e32e18a3df284d9bc172e91a85f826e5
mpartificer <megan.forsey@hotmail.com>
2025-01-29 11:12:41 -0330

    flatbed???? not this again

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,6 @@
 import Truck from './truck.svg';
 import Van from './van.svg';
-import Flatbed from './flatbed.svg';
+import Flatbed from './flatbed.svg?react';
 import Car from './car.svg?react';
 
 export {
