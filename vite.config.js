dcafd2c265edb667286c4702a9e9908eedde9060
mpartificer <megan.forsey@hotmail.com>
2025-01-29 11:32:04 -0330

    Updated vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react(), svgr({
-    svgrOptions: {
-      exportType: 'named',
-      ref: true,
-      svgo: false,
-      titleProp: true,
-    },
-  }),],
+  plugins: [
+    react(),
+    svgr({
+      exportType: 'default', // This is important
+      svgrOptions: {
+        icon: true,
+        svgo: true,
+        jsx: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src'
