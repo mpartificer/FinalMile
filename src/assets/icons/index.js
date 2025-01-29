4da19698d47073280def1e7f8e8cced6aba7ade3
mpartificer <megan.forsey@hotmail.com>
2025-01-29 11:15:31 -0330

    trying questions marks everywhere

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,5 @@
-import Truck from './truck.svg';
-import Van from './van.svg';
+import Truck from './truck.svg?react';
+import Van from './van.svg?react';
 import Flatbed from './flatbed.svg?react';
 import Car from './car.svg?react';
 
