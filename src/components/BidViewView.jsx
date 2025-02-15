6b5fe112631dcd24f9ff4b0f009f0f780c0043da
mpartificer <megan.forsey@hotmail.com>
2025-02-14 23:41:11 -0330

    forgot memo import

--- src/components/BidViewView.jsx
+++ src/components/BidViewView.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, memo } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { supabase } from '../../supabaseClient.js';
 import Header from './Header.jsx';
