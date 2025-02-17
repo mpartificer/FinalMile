0c31496583b850667aa2124b794e7e98eb3e4aa1
mpartificer <megan.forsey@hotmail.com>
2025-02-17 11:28:38 -0330

    Just trying to get lottie to work hehe

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -8,7 +8,7 @@ const LoadingOverlay = ({ message = "Processing..." }) => {
       <div className="bg-white rounded-lg p-8 max-w-md w-full text-center">
         <div className="w-64 h-64 mx-auto mb-4 flex items-center justify-center">
         <DotLottieReact 
-  src="../assets/icons/truckanimation.json"  // Note: file should be in public directory
+src='https://lottie.host/e7b2fb51-5851-46b0-8cc3-02dc1646b090/pKacEGePHp.lottie'
   loop 
   autoplay 
 />        </div>
