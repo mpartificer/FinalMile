a3587556091a9d727fa201074bd674cbbe9d6acf
mpartificer <megan.forsey@hotmail.com>
2025-02-18 11:42:26 -0330

    Slightly smaller shadow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="navbar headerBackground shadow-md text-primary-content fixed top-0 left-0 border-b border-gray-200">
+    <div className="navbar headerBackground shadow-sm text-primary-content fixed top-0 left-0 border-b border-gray-200">
       <Link to="/FinalMile/" className="pl-4 z-100 font-semibold text-xl hover:text-primary-content/80">
         Final Mile
       </Link>
