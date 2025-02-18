2b6661369621d58c6e49efd9104d241d8a90d144
mpartificer <megan.forsey@hotmail.com>
2025-02-18 10:57:42 -0330

    Updated header background color and bottom border

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="navbar headerBackground text-primary-content fixed top-0 left-0">
-      <Link to="/FinalMile/" className="pl-4 z-100 font-semibold text-xl hover:text-primary-content/80">
+      <Link to="/FinalMile/" className="pl-4 z-100 border-b border-gray-200 font-semibold text-xl hover:text-primary-content/80">
         Final Mile
       </Link>
     </div>
